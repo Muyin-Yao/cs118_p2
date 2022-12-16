63784a6c56fca40642b5becbe0f5713cb8366d95
Retr0ve <zhaodacheng@hotmail.com>
2022-12-15 22:15:26 -0500

    add sqls

--- d2.sql
+++ d2.sql
@@ -1,4 +1,6 @@
-INSERT OVERWRITE DIRECTORY "s3://{OUTPUT2}"
-SELECT pagename, popularity_trend 
+INSERT OVERWRITE DIRECTORY "s3://hiveoutput118/outputtwo/"
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t'
+SELECT page_name, popularity_trend 
 FROM step02_output 
 ORDER BY popularity_trend DESC LIMIT 100;
--- step.sql
+++ step.sql
@@ -1,3 +1,31 @@
+DROP TABLE IF EXISTS step02_output;
+CREATE TABLE step02_output (
+    page_name STRING, 
+    access_date STRING, 
+    page_view STRING,
+    total_pageview INT,
+    popularity_trend INT,
+    pageviews STRING) 
+ROW FORMAT DELIMITED 
+FIELDS TERMINATED BY '\t' 
+LINES TERMINATED BY '\n' 
+STORED AS TEXTFILE
+LOCATION "s3://outputstep2/outputall2/";
+
+INSERT OVERWRITE DIRECTORY "s3://hiveoutput118/outputone/"
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t'
+SELECT page_name, total_pageview 
+FROM step02_output 
+ORDER BY total_pageview DESC LIMIT 100;
+
+INSERT OVERWRITE DIRECTORY "s3://hiveoutput118/outputtwo/"
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t'
+SELECT page_name, popularity_trend 
+FROM step02_output 
+ORDER BY popularity_trend DESC LIMIT 100;
+
 INSERT OVERWRITE DIRECTORY "s3://hiveoutput118/outputthree/champion/"
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY '\t'
